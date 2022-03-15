b817b1c571472afabac547c678ce0e865e4256cc
Joshua Palis <jpalis@amazon.com>
2022-03-15 00:44:23 +0000

    added unit test file to test directory
    Signed-off-by: Joshua Palis <jpalis@amazon.com>

---
+++ src/test/java/TestMainScript.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+import org.opensearch.test.OpenSearchTestCase;
+
+public class TestMainScript extends OpenSearchTestCase{
+
+
+    // test considerations :
+    // - transport service is set up correctly
+    // - test that connection is up
+    // - mock a call to the transport services
+
+    @Test
+    public void unitTest(){
+        
+    }
+}
