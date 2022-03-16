186a07f2bffad7156a6c3e768a27c606ba8e4728
Joshua Palis <jpalis@amazon.com>
2022-03-16 00:44:21 +0000

    added test file
    Signed-off-by: Joshua Palis <jpalis@amazon.com>

--- src/test/java/TestMainScript.java
+++ src/test/java/TestMainScript.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 import org.opensearch.test.OpenSearchTestCase;
 
+import transportservice.RunPlugin;
+
 public class TestMainScript extends OpenSearchTestCase{
 
 
@@ -9,8 +11,24 @@ public class TestMainScript extends OpenSearchTestCase{
     // - test that connection is up
     // - mock a call to the transport services
 
+
+    // runplugin has no methods besides main, what should i test?
+    // 1) Mock node creation
+    // 2) check local ephemeral
+    // 3) check socket
+    // 4) test : once node is connected, check "Tcp transport channel accepted"
+    // - test main script debug output
+    // -- check for bound address comnfirmation
+
     @Test
     public void unitTest(){
         System.out.println("TEST : Unit Test is envoked");
+
+
+        // invoke main script for Transport service set up
+        // String[] args = null;
+        // RunPlugin.main(args);
+        
+
     }
 }
