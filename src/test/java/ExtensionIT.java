a9bb862fa3da8371cfd5d260dbae5f95f6a9cbde
Joshua Palis <jpalis@amazon.com>
2022-03-28 23:52:58 +0000

    updated runplugin to match main and added first integration test for netty transport address exposure
    Signed-off-by: Joshua Palis <jpalis@amazon.com>

--- src/test/java/ExtensionIT.java
+++ src/test/java/ExtensionIT.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Test;
+import org.opensearch.common.component.Lifecycle;
+import org.opensearch.common.network.NetworkAddress;
 import org.opensearch.common.settings.Settings;
+import org.opensearch.common.transport.TransportAddress;
 import org.opensearch.test.OpenSearchIntegTestCase;
 import org.opensearch.transport.TransportSettings;
 
@@ -7,47 +10,48 @@ import transportservice.RunPlugin;
 import transportservice.netty4.Netty4Transport;
 import org.opensearch.threadpool.ThreadPool;
 
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-
 public class ExtensionIT extends OpenSearchIntegTestCase {
 
     // integration tests
+    // - test transport information is exposed
     // - test transport request / response
     // - (SDK transport) test handshake request recieved (message "internal:tcp/handshake recieved request")
     // - (SDK transport) test handshake request response sent (message : internal:tcp/handshake sent response)
     // - (SDK action listener) test action listener work
-    // - test tcp handshake connection reset
-    // - test tcp handshake timeout
-    // - test profiles
-    // - test profiles include default
-    // - test
 
-    private Settings settings = Settings.builder()
-        .put("node.name", "NettySizeHeaderFrameDecoderTests")
-        .put(TransportSettings.BIND_HOST.getKey(), "127.0.0.1")
-        .put(TransportSettings.PORT.getKey(), "9301")
-        .build();
-
+    private String address = "127.0.0.1";
+    private int port = 9301;
 
     @Test
     public void testThatInfosAreExposed() {
 
         RunPlugin runPlugin = new RunPlugin();
         ThreadPool threadPool = new TestThreadPool("test");
+        Settings settings = Settings.builder()
+            .put("node.name", "IntegrationTests")
+            .put(TransportSettings.BIND_HOST.getKey(), address)
+            .put(TransportSettings.PORT.getKey(), port)
+            .build();
 
-        System.out.println(TransportSettings.PORT.getKey());
-
-        // ensure that profile addresses are exposed
+        // start netty transport and ensure that address info is exposed
         try (Netty4Transport transport = startNettyTransport(runPlugin.getNetty(settings, threadPool))) {
-            
-            assertEquals(1, transport.profileBoundAddresses().size());
-            assertEquals(1, transport.boundAddress().boundAddresses().length);
+
+            // check bound addresses
+            for (TransportAddress transportAddress : transport.boundAddress().boundAddresses()) {
+                assert (transportAddress instanceof TransportAddress);
+                assertEquals(address, transportAddress.getAddress());
+                assertEquals(port, transportAddress.getPort());
+            }
+
+            // check publish addresses
+            assert (transport.boundAddress().publishAddress() instanceof TransportAddress);
+            TransportAddress publishAddress = transport.boundAddress().publishAddress();
+            assertEquals(address, NetworkAddress.format(publishAddress.address().getAddress()));
+            assertEquals(port, publishAddress.address().getPort());
+
         } finally {
             terminate(threadPool);
         }
-
-        // ensure that bound transport addresses
     }
 
     @Test
@@ -80,6 +84,13 @@ public class ExtensionIT extends OpenSearchIntegTestCase {
     @Test
     public void testTcpHandshakeTimeout() {
 
+    }
+
+    // helper method to ensure netty transport was started
+    private Netty4Transport startNettyTransport(Netty4Transport transport) {
+        transport.start();
+        assertEquals(transport.lifecycleState(), Lifecycle.State.STARTED);
+        return transport;
     }
 
 }
